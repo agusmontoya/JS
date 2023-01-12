a63e76f290b5ecef4a2837151ff92211f44b4ab3
agusmontoya <agustinamontoya1999@gmail.com>
2023-01-12 17:29:40 -0300

    agrego primera parte de mi ecommerce

--- js.js
+++ js.js
@@ -1,38 +1,18 @@
-let producto = "";
-let precio = 0;
-let cantidad = 0;
-let cantidadTotal = 0;
-let seguirComprando = false;
-let totalCompra = 0;
+alert ("Bienvenido a nuestra tienda online")
 
-do{
-    producto = prompt ("¿Qué prenda de nuestro catálogo querés comprar?")
-    cantidad = parseInt (prompt ("¿Cuántas prendas vas a llevar?"))
+const carrito = []
 
-    while (Number.isNaN (cantidad) || cantidad === 0) {
-        if (cantidad !== 0) {
-            alert ("Por favor elegí un número")
-        } else {
-            alert ("Por favor ingrese un número que no sea cero")
-        }
-        cantidad = parseInt(promp ("¿Cuántas prendas vas a llevar?"))
-    }
+const orderarPrecioMenor = () => {
+    listaPrendasDisponibles.sort ( (a,b) => a.precio - b.precio)
+    mostarLista ()
+}
 
-    switch (producto){
-        case "vestido lila":
-            precio = 1200;
-            break;
-        case "pollera escosesa":
-            precio = 2000;
-            break;
-        case "pantalon palazo":
-            precio = 3500;
-            break;
-        default:
-            alert("Debes elegir una prenda de nuestro catálogo")
-            precio = 0
-            cantidad = 0;
-    }
+const mostarLista = () => {
+    
+    const listaOrdenada = listaPrendasDisponibles.map (prendas => {
+        return prendas.nombre + " " + prendas.precio
+    })
+    alert ("Stock disponible:" + "\n\n" + listaOrdenada)
+}
 
-    seguirComprando = confirm("Seguir comprando")
-} while (seguirComprando)
+orderarPrecioMenor()
---
+++ stock.js
@@ -0,0 +1,41 @@
+const listaPrendasDisponibles = [
+    {
+        id:1,
+        nombre: "vestido",
+        color: "rojo",
+        talle: "S",
+        precio: 1000,
+        cantidad: 0,
+    },
+
+    {
+        id:2,
+        nombre: "campera",
+        color: "azul",
+        talle: "M",
+        precio: 1500,
+        cantidad: 0,
+    },
+
+    {
+        id:3,
+        nombre: "pantalon",
+        color: "negro",
+        talle: "S",
+        precio: 1000,
+        cantidad:0,
+    },
+
+    {
+        id:4,
+        nombre: "blazer",
+        color: "blanco",
+        talle: "S",
+        precio: 600,
+        cantidad:0,
+    },
+    
+    
+
+    
+]
